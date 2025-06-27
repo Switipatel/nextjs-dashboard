56c1e3466a42fe6cf9d3eb0abcd4a438e88d50ab
Switi Patel <switi.patel@silvertouch.com>
2025-06-27 18:10:20 +0530

    Connect  with Vercel Account

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+ export default function CustomersPage() {
+  return (
+    <div>
+      <h1>Customers Page</h1>
+      <p>This is the customers page of the dashboard.</p>
+    </div>
+  )
+};
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function InvoicesPage() {
+  return (
+    <div>
+      <h1>Invoices Page</h1>
+      <p>This is the invoices page of the dashboard.</p>
+    </div>
+  );
+}
